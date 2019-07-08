cf923f4c2269f8660b8d4da00e7b5abc77b870e8
OtacilioN <otacilio.n97@gmail.com>
2019-07-08 11:50:10 -0300

    Updates

---
+++ precache-manifest.42de1ba0a4c1a7970154d7676d5d0267.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f76b4baceca1fc6ae701d835febfe2c",
+    "url": "/CodigoDeConduta/index.html"
+  },
+  {
+    "revision": "cca65fc6114a7ba57477",
+    "url": "/CodigoDeConduta/static/css/main.0d16e6b7.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/CodigoDeConduta/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "cca65fc6114a7ba57477",
+    "url": "/CodigoDeConduta/static/js/main.1362d79a.chunk.js"
+  },
+  {
+    "revision": "d097eb44d961e4801a6b",
+    "url": "/CodigoDeConduta/static/js/runtime~main.0f67889f.js"
+  }
+]);
