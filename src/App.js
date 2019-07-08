6738db234ec173c762fda8a387133b6d5ef740d5
OtacilioN <otacilio.n97@gmail.com>
2019-07-08 11:44:33 -0300

    Add codigo de conduta text

--- src/App.js
+++ src/App.js
@@ -1,23 +1,247 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Código de Conduta Facebook Developers Circle Recife</h1>
+        <section id="objetivo">
+          <a style={{ color: "white" }} href="#objetivo">
+            <h2>1. Objetivo</h2>
+          </a>
+          <h4>
+            O objetivo principal do Facebook Developers Circle Recife é o de ser
+            inclusivo ao maior número de pessoas, com as mais variadas e
+            diversas histórias de vida possíveis. Como tal, estamos empenhados
+            em proporcionar um ambiente amigável, seguro e acolhedor para todos,
+            independentemente do sexo, identidade de gênero, orientação sexual,
+            habilidade, etnia, status socioeconômico e religião (ou falta dela).
+            Este código de conduta descreve as nossas expectativas para todos
+            aqueles que participam da nossa comunidade, bem como as
+            consequências para um comportamento inaceitável. Convidamos todos
+            aqueles que participam no Facebook Developers Circle Recife para nos
+            ajudar a criar experiências seguras e positivas para todos.
+          </h4>
+        </section>
+        <section id="comportamento-esperado">
+          <a style={{ color: "white" }} href="#comportamento-esperado">
+            <h2>2. Comportamento Esperado</h2>
+          </a>
+          <h4>
+            Os seguintes comportamentos são esperados e solicitados a todos os
+            membros da comunidade:
+            <ul>
+              <li>
+                Participe de uma forma autêntica e ativa. Ao fazê-lo, você
+                contribui para a saúde e longevidade desta comunidade.
+              </li>
+              <li>Exercite a empatia e o respeito no seu discurso e ações.</li>
+              <li>Tente a colaboração antes do conflito.</li>
+              <li>
+                Abstenha-se de um comportamento degradante, discriminatório ou
+                assediante.
+              </li>
+              <li>
+                Tenha atenção ao seu entorno e aos seus colegas participantes.
+                Alerte à liderança da comunidade se você notar uma situação
+                perigosa, alguém em perigo ou violações deste Código de Conduta,
+                mesmo que pareçam inconsequentes.
+              </li>
+              <li>
+                Lembrem-se que os locais de eventos muitas vezes são públicos,
+                respeitem as regras e todas as pessoas presentes nestes locais.
+              </li>
+            </ul>
+          </h4>
+        </section>
+        <section id="comportamento-inaceitavel">
+          <a style={{ color: "white" }} href="#comportamento-inaceitavel">
+            <h2>3. Comportamento inaceitável</h2>
+          </a>
+          <h4>
+            Os seguintes comportamentos são considerados assédio e são
+            inaceitáveis em nossa comunidade:
+            <ul>
+              <li>
+                Violência, ameaças de violência ou linguagem violenta dirigida
+                contra outra pessoa.
+              </li>
+              <li>
+                Discursos sexualistas, racistas, homofóbicos, transfóbicos ou de
+                outras formas discriminatórias.
+              </li>
+              <li>
+                Publicar ou exibir de material sexualmente explícito ou violento
+                em quaisquer canais do Facebook Developers Circle Recife. -
+                Publicar ou ameaçar a publicação de material pessoal privado. -
+                Insultos pessoais, particularmente aqueles relacionados ao
+                gênero, orientação sexual, raça, religião ou deficiência.
+              </li>
+              <li>
+                Contato físico inapropriado. Você deve ter o consentimento de
+                alguém antes de tocá-lo.
+              </li>
+              <li>
+                Intenção sexual indesejada. Isso inclui comentários sexuais ou
+                piadas; movimentos e toques inapropriados, tentativas e avanços
+                sexuais não consensuais.
+              </li>
+              <li>
+                Intimidação deliberada, perseguição ou stalking (on-line ou
+                pessoalmente)
+              </li>
+              <li>Icentivar qualquer comportamento acima.</li>
+            </ul>
+          </h4>
+        </section>
+        <section id="consequencias">
+          <a style={{ color: "white" }} href="#consequencias">
+            <h2>4. Consequências de comportamento inaceitável</h2>
+          </a>
+          <h4>
+            O comportamento inaceitável de qualquer membro da comunidade,
+            incluindo patrocinadores e aqueles com autoridade de decisão, não
+            será tolerado. Qualquer pessoa que solicitar para que um
+            comportamento inaceitável seja parado deve ser atendida
+            imediatamente. Se um membro da comunidade se envolver em um
+            comportamento inaceitável, os organizadores da comunidade podem
+            tomar as medidas que julgarem apropriadas, até e incluindo uma
+            proibição temporária ou expulsão permanente da comunidade sem aviso
+            prévio (e sem reembolso no caso de um evento pago).
+          </h4>
+        </section>
+        <section id="denuncias">
+          <a style={{ color: "white" }} href="#denuncias">
+            <h2>5. Guia de denúncias</h2>
+          </a>
+          <h4>
+            Se você está sujeito ou testemunha um comportamento inaceitável, ou
+            tem outras preocupações, notifique um organizador da comunidade o
+            mais rápido possível. Através dos emails, selecione o email de um
+            dos organizadores em que você se sinta mais confortável para
+            realizar a denúncia:
+            <ul>
+              <li>
+                Caroline Dalmolin (Program Manager) -{" "}
+                <a style={{ color: "white" }} href="mailto:caroldalmo@fb.com">
+                  caroldalmo@fb.com
+                </a>
+              </li>
+              <li>
+                Lívia Souza (Developer Circle Leader) -{" "}
+                <a
+                  style={{ color: "white" }}
+                  href="mailto:liviarsouza@gmail.com"
+                >
+                  liviarsouza@gmail.com
+                </a>
+              </li>
+              <li>
+                Otacilio Maia (Developer Circle Leader) -{" "}
+                <a
+                  style={{ color: "white" }}
+                  href="mailto:contato@otaciliomaia.com"
+                >
+                  contato@otaciliomaia.com
+                </a>
+              </li>
+              <li>
+                Paula Soares (Developer Circle Leader) -{" "}
+                <a
+                  style={{ color: "white" }}
+                  href="mailto:soaresdelapaula@gmail.com"
+                >
+                  soaresdelapaula@gmail.com
+                </a>
+              </li>
+              <li>
+                Thalita Albuquerque (Developer Circle Leader) -{" "}
+                <a
+                  style={{ color: "white" }}
+                  href="mailto:thallitaalbuquerque@gmail.com"
+                >
+                  thallitaalbuquerque@gmail.com
+                </a>
+              </li>
+              <li>
+                Thiago Augusto (Developer Circle Leader) -{" "}
+                <a
+                  style={{ color: "white" }}
+                  href="mailto:thiagomartinsthiagoaugusto@gmail.com"
+                >
+                  thiagomartinsthiagoaugusto@gmail.com
+                </a>
+              </li>
+            </ul>
+            Além disso, a liderança da comunidade está disponível para ajudar os
+            membros da comunidade a se envolverem com a aplicação da lei local
+            ou, de outra forma, ajudar aqueles que vivenciam comportamentos
+            inaceitáveis a se sentirem seguros. No contexto de evento, em
+            pessoa, os organizadores também fornecerão escoltas conforme
+            desejado pela pessoa que está necessitando.
+          </h4>
+        </section>
+        <section id="contestacoes">
+          <a style={{ color: "white" }} href="#contestacoes">
+            <h2>6. Contestações</h2>
+          </a>
+          <h4>
+            Se você acha que foi acusado falsamente ou injustamente de violar
+            este Código de Conduta, você deve notificar o Facebook Developers
+            Circle Recife com uma descrição concisa de sua queixa. Sua queixa
+            será avaliada e respondida o mais brevimente possível.
+          </h4>
+        </section>
+        <section id="consideracoes">
+          <a style={{ color: "white" }} href="#consideracoes">
+            <h2>7. Considerações</h2>
+          </a>
+          <h4>
+            Esperamos que todos os participantes da comunidade (contribuidores,
+            pagos ou de outra forma, patrocinadores e outros convidados)
+            respeitem este Código de Conduta em todos os locais da comunidade
+            (on-line e em pessoa) bem como em todas as comunicações individuais
+            em relação aos assuntos da comunidade. Este código de conduta e seus
+            procedimentos relacionados também se aplicam a comportamentos
+            inaceitáveis que ocorrem fora do alcance das atividades comunitárias
+            quando esse comportamento tem potencial para afetar adversamente a
+            segurança e o bem-estar dos membros da comunidade.
+          </h4>
+        </section>
+        <section id="licenca">
+          <a style={{ color: "white" }} href="#licenca">
+            <h2>9. Licença e Atribuições</h2>
+          </a>
+          <h4>
+            Este códido de conduta está licenciado sob a licensa{" "}
+            <a
+              style={{ color: "white" }}
+              href="https://creativecommons.org/licenses/by-sa/3.0/"
+            >
+              Creative Commons Attribution-ShareAlike
+            </a>{" "}
+            e é baseado no{" "}
+            <a style={{ color: "white" }} href="https://github.com/recihub/">
+              código de conduta ReciHub.
+            </a>{" "}
+            Partes deste texto foram retiradas e adaptadas do{" "}
+            <a
+              style={{ color: "white" }}
+              href="https://www.djangoproject.com/conduct/"
+            >
+              Código de conduta Django
+            </a>{" "}
+            e do{" "}
+            <a
+              style={{ color: "white" }}
+              href="http://geekfeminism.wikia.com/wiki/Conference_anti-harassment/Policy"
+            >
+              Geek Feminism Anti-Harassment Policy
+            </a>
+          </h4>
+        </section>
       </header>
     </div>
   );
